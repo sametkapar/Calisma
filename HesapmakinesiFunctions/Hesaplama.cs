6d2658a9bd7ed71b72e08b7951bbd70718bb46fb
sametkapar <130076921+sametkapar@users.noreply.github.com>
2024-07-20 12:12:52 +0300

    calisma

---
+++ HesapmakinesiFunctions/Hesaplama.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HesapmakinesiFunctions
+{
+    internal class Hesaplama
+    {
+
+        public double Topla(double sayi1, double sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+        public double Carp(double sayi1, double sayi2)
+        {
+            return sayi1 * sayi2;
+        }
+        public double Cikar(double sayi1, double sayi2)
+        {
+            return sayi1 - sayi2;
+
+        }
+        public double Bol(double sayi1, double sayi2)
+        {
+            double bol = 0;
+            if (sayi1 > sayi2 && sayi2 != 0)
+            {
+                bol = sayi1 / sayi2;
+            }
+            return bol;
+        }
+        public double UsAl(double taban, double us)
+        {
+            double sonuc = 1;
+            for (int i = 0; i < us; i++)
+            {
+                sonuc *= taban;
+            }
+            return sonuc;
+
+
+        }
+    }
+}
---
+++ HesapmakinesiFunctions/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HesapmakinesiFunctions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] islemler = { "Toplama", "Çıkarma", "Çarpma", "Bölme", "Üs alma" };
+
+            Console.WriteLine("-*-*-BASİT HESAP MAKİNESİ-*-*-");
+            Hesaplama nesne = new Hesaplama();
+
+            Console.Write("1. sayıyı giriniz: ");
+            double sayi1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("2. sayıyı giriniz: ");
+            double sayi2 = Convert.ToDouble(Console.ReadLine());
+           
+            
+            for (int i = 0; i < islemler.Length; i++)
+            {
+                Console.WriteLine((i+1) + " - " + islemler[i]);
+               
+            }
+
+            Console.WriteLine("İşlem numarası seçiniz");
+            int secim = Convert.ToInt32(Console.ReadLine());
+            switch (secim)
+            {
+                case 1:
+                    Console.WriteLine(sayi1 + " + " +sayi2 +" = " +nesne.Topla(sayi1, sayi2));
+                    break;
+                case 2:
+                    Console.WriteLine(sayi1 + " - " + sayi2 + " = " + nesne.Cikar(sayi1, sayi2));
+                    break;
+                case 3:
+                    Console.WriteLine(sayi1 + " * " + sayi2 + " = " + nesne.Carp(sayi1, sayi2));
+                    break;
+                case 4:
+                    Console.WriteLine(sayi1 + " / " + sayi2 + " = " + nesne.Bol(sayi1, sayi2));
+                    break;
+                case 5:
+                    Console.WriteLine(sayi1 + " ^ " + sayi2 + " = " + nesne.UsAl(sayi1, sayi2));
+                    break;
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
